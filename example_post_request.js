ffdff3d1751e1ddacf4c9c72fd89ef7cb58a5c79
juanda <juandacorreo@gmail.com>
2024-03-06 08:51:01 +0100

    add example post request

---
+++ example_post_request.js
@@ -0,0 +1,19 @@
+// Example POST method implementation:
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST', // GET por defecto
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+  return response.json() // parses JSON response into native JavaScript objects
+}
+
+// TODO: Replace 'token' with a valid token
+const token = 'adfadfadadfadfadsf'
+
+postData(`http://localhost:4000/movements?token=${token}`, {
+  amount: 100,
+  date: new Date().toISOString(),
+}).then((data) => {
+  console.log(data) // JSON data parsed by `data.json()` call
+})
